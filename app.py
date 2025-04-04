c8898d563a6769f71d105dc3913dae394642f1f6
trh4ckn0n <trhacknon@proton.me>
2025-04-05 01:23:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -89,3 +89,8 @@ def index():
 @app.route("/history")
 def history():
     return render_template("history.html", history=session.get("history", []))
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render fournit $PORT
+    app.run(host="0.0.0.0", port=port)
